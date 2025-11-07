e8603ce1c03c99b22e2bef4f3405412e92f43806
jaechang-hits <jaechang@hits.ai>
2025-11-07 11:11:00 +0900

    update

--- biomni_env/omics/install.sh
+++ biomni_env/omics/install.sh
@@ -1,6 +1,6 @@
-env_name="biomni_hits_omics"
-
-conda env create -n $env_name -f bio_env.yml
+env_name="hits_omics"
+conda install -n base mamba -c conda-forge
+conda env create -v -n $env_name -f bio_env.yml
 conda activate $env_name
 pip install -r requirements_langchain.txt
 pip install -r requirements.txt
