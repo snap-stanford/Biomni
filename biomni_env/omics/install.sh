20d3e91cae4a835d737a8b4d6353b4f1c837cbec
jaechang-hits <jaechang@hits.ai>
2025-11-07 11:19:39 +0900

    update

--- biomni_env/omics/install.sh
+++ biomni_env/omics/install.sh
@@ -1,11 +1,17 @@
 env_name="hits_omics"
-conda install -n base mamba -c conda-forge
-conda env create -v -n $env_name -f bio_env.yml
-conda activate $env_name
-pip install -r requirements_langchain.txt
-pip install -r requirements.txt
 
+# Install mamba if not already installed
+conda install -n base mamba -c conda-forge -y
+
+# Create environment with all dependencies (conda + pip) from unified YAML
+mamba env create -v -n $env_name -f bio_env.yml
+
+# Activate environment
+conda activate $env_name
+
+# Install R packages if needed
 conda env update --file r_packages.yml
 Rscript install_r_packages.R
 
+# Install the main package in editable mode
 pip install -e ../../../
