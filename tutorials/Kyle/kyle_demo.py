b983b97d8c34021115faefd6e3fb270f0d9989e1
KyleAtASU <shunjieh@asu.edu>
2026-02-17 02:21:00 -0800

    Uncomment query for PubMed papers about CRISPR in demo script

--- tutorials/Kyle/kyle_demo.py
+++ tutorials/Kyle/kyle_demo.py
@@ -19,9 +19,9 @@ agent = A1(
 # agent.go("Perform scRNA-seq annotation at [PATH] and generate meaningful hypothesis")
 # agent.go("Predict ADMET properties for this compound: CC(C)CC1=CC=C(C=C1)C(C)C(=O)O")
 
-agent.go("""Plan a CRISPR screen to identify genes that regulate T cell exhaustion,
-        measured by the change in T cell receptor (TCR) signaling between acute
-        (interleukin-2 [IL-2] only) and chronic (anti-CD3 and IL-2) stimulation conditions.
-        Generate 32 genes that maximize the perturbation effect.""")
+# agent.go("""Plan a CRISPR screen to identify genes that regulate T cell exhaustion,
+#         measured by the change in T cell receptor (TCR) signaling between acute
+#         (interleukin-2 [IL-2] only) and chronic (anti-CD3 and IL-2) stimulation conditions.
+#         Generate 32 genes that maximize the perturbation effect.""")
 
-# agent.go("Query PubMed for papers about CRISPR and return 5 results.")
+agent.go("Query PubMed for papers about CRISPR and return 5 results.")
